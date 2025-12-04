8879f746ea24574c5e927bd6ad4c4d2289289103
Wannabeali <aliqureshi755@gmail.com>
2025-12-04 11:50:07 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,28 +1,3 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $name = htmlspecialchars($_POST["name"]);
-    $email = htmlspecialchars($_POST["email"]);
-    $subject = htmlspecialchars($_POST["subject"]);
-    $message = htmlspecialchars($_POST["message"]);
-
-    $to = "aliqureshi755@gmail.com";  // your email
-    $headers = "From: $name <$email>\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-
-    $body = "
-        <h2>New Contact Message</h2>
-        <p><strong>Name:</strong> $name</p>
-        <p><strong>Email:</strong> $email</p>
-        <p><strong>Subject:</strong> $subject</p>
-        <p><strong>Message:</strong><br>$message</p>
-    ";
-
-    if (mail($to, $subject, $body, $headers)) {
-        echo "success";
-    } else {
-        echo "error";
-    }
-}
+echo "sendmail.php is working. Method: " . $_SERVER['REQUEST_METHOD'];
 ?>
